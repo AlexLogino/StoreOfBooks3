f51679bcfc8a5c241a98040b1eeebd527b18c888
AlexLogino <alexeyjob3@gmail.com>
2024-10-05 16:58:00 +0300

    add App

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Открытие магазина ожидается через 2 месяца");
+        System.out.println("Открытие магазина ожидается через 4 дня");
+        System.out.println("Открытие магазина ожидается через 3 месяца");
+        System.out.println("Открытие магазина ожидается через 1 день");
+    }
+}
